12dc4680260b30e4bcb7251cbaa006c5f2e3a7fc
Guy Strelitz <guy@sodiumskies.com>
2025-09-04 16:05:53 +0100

    tightened the regex pattern

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const semVerPattern = /^(\d+\.\d+\.)(\d+)/;
+const semVerPattern = /^(\d+\.\d+\.)(\d+)$/;
 
 try {
   // Read the target file
